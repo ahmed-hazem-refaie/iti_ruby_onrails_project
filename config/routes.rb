3d27d45c3648776def8bc87e1622dcece523713e
Mahmoud <mahmoedmohamed19@gmail.com>
2020-04-23 00:00:23 +0200

    fix sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root :to => "home#home"
   resources :notifications
   resources :friendships
   resources :orderdetails
   resources :groups
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :activites
-  devise_for :users#, :controllers => { :omniauth_callbacks => “users/omniauth_callbacks” }
   resources :orders
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
