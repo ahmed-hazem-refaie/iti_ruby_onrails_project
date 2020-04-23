57bf618f75f6a70a5596508080fc1d9dc525f012
Yassminelbialy <Yassminelbialy@gmail.com>
2020-04-23 19:28:15 +0200

    change null with true

--- db/migrate/20200418234843_create_friendships.rb
+++ db/migrate/20200418234843_create_friendships.rb
@@ -1,9 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
+      t.references :friend, null: true, foreign_key: true
+      t.references :group, null: true, foreign_key: true
 
       t.timestamps
     end
