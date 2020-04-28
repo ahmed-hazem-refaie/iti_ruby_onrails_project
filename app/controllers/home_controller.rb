cf9f1d700e256d2728afc34702b2a221500b58e7
Mahmoud <mahmoedmohamed19@gmail.com>
2020-04-28 11:06:08 +0200

    hot fix for home orders query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
     include Devise::Controllers::Helpers 
     def index
         @activities = Hash.new
-        @my_orders = Order.find_by from: current_user.id
+        @my_orders = Order.where user_id: current_user.id
+        print "*******************"
+        print @my_orders
         current_user.friends.each do |friend|
             @activities[friend] = friend.activites
         end
