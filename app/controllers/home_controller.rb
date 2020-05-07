b861f0a4d7a9231ad489e5ccc3ec9aa3041e6aaa
MacBook Air <macbookair@MacBook-Air.local>
2020-05-07 11:05:41 +0200

    notofication view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
     include Devise::Controllers::Helpers 
     def index
         @activities = Hash.new
+        @my_notification = User.find(1).notifications
         @my_orders = Order.where user_id: current_user.id
         print "*******************"
         print @my_orders
